e987f863671d6152871965a50099547ec6ac2138
kenjihiraoka <willian.hiraoka@fatec.sp.gov.br>
2020-06-13 21:48:18 -0300

    send messages to kafka topic

--- src/twitter_full.py
+++ src/twitter_full.py
@@ -1,9 +1,8 @@
 import sys
-import json
 import requests
 
-from pprint import pprint
 from authentication import Authentication
+from producer.kafka_producer import KafkaEventProducer
 
 sys.path.append(".")
 import twitter_env as env
@@ -12,14 +11,16 @@ STREAM_URL = "https://api.twitter.com/labs/1/tweets/stream/sample"
 
 
 class TwitterFull:
-    @staticmethod
-    def stream_connect(authentication):
+    def __init__(self):
+        self.producer = KafkaEventProducer(bootstrap_servers=["localhost:9092"])
+
+    def stream_connect(self, authentication):
         with requests.get(STREAM_URL, auth=authentication,
                           headers={"User-Agent": "TwitterDevSampledStreamQuickStartPython"},
                           stream=True) as response:
             for response_line in response.iter_lines(chunk_size=20):
                 if response_line:
-                    pprint(json.loads(response_line))
+                    self.producer.send_massage("general-tweets", response_line)
 
 
 if __name__ == "__main__":
